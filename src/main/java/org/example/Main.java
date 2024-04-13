57e90681a2edaca5daee18898f6f7e411aae218b
Shadozx <74901858+Shadozx@users.noreply.github.com>
2024-04-13 18:42:53 +0300

    add Person to Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,32 @@
 package org.example;
 
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void showInfo() {
+        System.out.printf("Person with name - %s and has age - %d%n", name, age);
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
+        Person person = new Person("Tom", 25);
 
+        person.showInfo();
     }
 }
